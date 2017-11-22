517d4f6a5f7f6fd8e9e35d2ea8cfb9253697fa02
boctavian96 <boctavian96@yahoo.com>
2017-11-22 11:34:02 +0200

    Chain Resp

---
+++ src/sabloane1/chainofresponsability/IMG_XY.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sabloane1.chainofresponsability;
+
+/**
+ *
+ * @author octavian
+ */
+public class IMG_XY implements Image{
+
+    @Override
+    public String doSomething() {
+        return "IMG_XY";
+    }
+    
+    
+    
+}
